0d48a62baf376b7850fa6a4aee074ab1dc9583aa
Dien Vu <meotimdihia@gmail.com>
2012-12-23 17:57:55 +0700

    Updated

---
+++ README.php
@@ -0,0 +1,12 @@
+Edit file trực tiếp từ browser để test bất cứ gì mới hoặc simple về php. 
+Very recommend for php newbie hoặc junior PHP developer.
+If you are a senior developer , you may still need it.
+
+###REQUIRED: 
+	- Git
+###INSTALLING:
+	- git pull git@github.com:meotimdihia/edit.git
+	- git submodule foreach git pull
+
+###PREVIEW
+![Edit file from browser](http://i.stack.imgur.com/vk4w8.jpg)
