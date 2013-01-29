3feee9c37b5215f96d3f6a6c91476da9415dbaae
Dien Vu <meotimdihia@gmail.com>
2013-01-29 22:50:55 +0700

    u

---
+++ dates.php
@@ -0,0 +1,28 @@
+<div style='width:500px;margin:auto'>
+	<center>
+<?php
+$from = strtotime('-10 days');
+$to = strtotime('+10 days');
+while ($from < $to) {
+	echo date ("Y-m-d", $from) . " - " . strtotime(date ("Y-m-d", $from)) . "<br/>";
+	$from += 86400;
+}
+?>
+</center>
+<br/>
+<form>
+		<label>Timestamp</label>
+		<input type='text' name='timestamp' />
+		<label>Date</label>
+		<input type='text' name='date' />
+		<input type='submit' />
+</form>
+<?php
+if (!empty($_GET['timestamp'])) {
+	echo 'Timestamp to date: <strong>' . date('d-m-Y H:i:s', $_GET['timestamp']) . '</strong><br/>';
+}
+if (!empty($_GET['date'])) {
+	echo 'Date to timestamp: <strong>' . strtotime($_GET['date']) . '</strong>';
+}
+?>
+</div>
